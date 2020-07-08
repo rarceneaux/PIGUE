676c6badee77469293ba99075444eed6fae8d69b
Raymond <raymond.techcoder@gmail.com>
2020-07-07 23:34:41 -0500

    added all packages and 1 play to database

---
+++ Models/Play.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIGUE.Models
+{
+    public class Play
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int FormationId { get; set; }
+    }
+}
