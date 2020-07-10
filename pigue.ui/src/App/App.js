1e9f66557260cfe66a01f8e3bf4d5ea962a044bc
Raymond <raymond.techcoder@gmail.com>
2020-07-10 08:16:42 -0500

    wip

---
+++ pigue.ui/src/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>PIGUE</h2>
+        <button className="btn btn-info"><i className="fas fa-rocket">GAMETIME</i></button>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
