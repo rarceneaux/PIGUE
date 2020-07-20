32379b349328dd405bed7cde792ed77bc5b779ba
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-07-20 12:41:32 -0500

    updated api

---
+++ Models/PlayPlayers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIGUE.Models
+{
+    public class PlayPlayers
+    {
+        public int Id { get; set; }
+        public int PlayId { get; set; }
+        public int PlayerId { get; set; }
+    }
+}
