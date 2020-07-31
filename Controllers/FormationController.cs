6477baeb0aac95eb834f3d4e11d81f9f5a37bfb9
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-07-31 17:54:09 -0500

    backend formation changes

--- Controllers/FormationController.cs
+++ Controllers/FormationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PIGUE.DataAccess;
+using PIGUE.Models;
 
 namespace PIGUE.Controllers
 {
@@ -16,12 +17,20 @@ namespace PIGUE.Controllers
             var allFormations = _formationRepository.GetAllFormations();
             return Ok(allFormations);
         }
+
         [HttpGet("{formationId}")]
         public IActionResult GetFormationById(int formationId)
         {
             var formation = _formationRepository.GetFormationById(formationId);
             if (formation == null) return NotFound("Illegal Formation On The Offense.");
             return Ok(formation);
+        }
+
+        [HttpPost]
+        public IActionResult AddNewFormation(Formations formationToAdd)
+        {
+            var formationAdded = _formationRepository.AddNewFormation(formationToAdd);
+            return Created("", formationAdded);
         }
     }
 }
